bd301932e0e27516de846ba4bcc5d24a990a99bc
ZiemowitKorz <106156554+ZiemowitKorz@users.noreply.github.com>
2022-06-09 13:41:17 +0200

    Add files via upload

---
+++ Basics/SwitchinLetters/SwitchinLetters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace exercises
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test("abcd"));
+            Console.WriteLine(test("a"));
+            Console.WriteLine(test("xy"));
+            Console.ReadLine();
+        }
+        public static string test(string str)
+        {
+            if (str.Length > 1)
+            {
+                return str.Substring(str.Length - 1) + str.Substring(1, str.Length - 2) + str.Substring(0, 1);
+
+            }
+            else 
+            {
+                return str;
+            }
+           
+        }
+    }
+}
