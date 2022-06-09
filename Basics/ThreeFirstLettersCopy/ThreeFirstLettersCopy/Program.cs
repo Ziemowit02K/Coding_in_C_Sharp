c931459266ffef8377eb2c1a2dcec4b8776bda1e
ZiemowitKorz <106156554+ZiemowitKorz@users.noreply.github.com>
2022-06-09 14:12:28 +0200

    Add files via upload

---
+++ Basics/ThreeFirstLettersCopy/ThreeFirstLettersCopy/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution("Python",2));
+            Console.WriteLine(Solution("Python", 3));
+            Console.WriteLine(Solution("JS", 3));
+            Console.ReadLine();
+        }
+        public static string Solution(string str,int i)
+        {
+            string result = string.Empty;
+            int frontOfTheString = 3;
+
+            if (frontOfTheString > str.Length) 
+                frontOfTheString = str.Length;
+
+            string front = str.Substring(0,frontOfTheString);
+
+            for (int n = 0; n < i; n++)
+            {
+                result += front;
+            }
+            return result;
+
+            
+
+        }
+    }
+}
