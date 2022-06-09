84aec5f36163f447350fc1c2f5691fea8fe3ff8e
ZiemowitKorz <106156554+ZiemowitKorz@users.noreply.github.com>
2022-06-09 13:39:05 +0200

    Add files via upload

---
+++ Basics/IF/IF/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+    Console.WriteLine("Podaj dwie cyrfy: ");
+    int n1 = int.Parse(Console.ReadLine());
+    int n2 = int.Parse(Console.ReadLine());
+
+   
+
+    Console.WriteLine("Podaj dwie cyrfy: ");
+    int n3 = int.Parse(Console.ReadLine());
+    int n4 = int.Parse(Console.ReadLine());
+
+    
+
+    Console.WriteLine("Podaj dwie cyrfy: ");
+    int n5 = int.Parse(Console.ReadLine());
+    int n6 = int.Parse(Console.ReadLine());
+
+
+
+
+if (n1 == 30 || n2 == 30 || n1 + n2 == 30)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+
+
+if (n3 == 30 || n4 == 30 || n3 + n4 == 30)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+
+
+if (n5 == 30 || n6 == 30 || n5 + n6 == 30)
+    {
+        Console.WriteLine("True");
+    }
+    else
+    {
+        Console.WriteLine("False");
+    }
+
+    Console.ReadKey();
+
+
+
+
---
+++ Basics/If, różnica pomiędzy n a 51/If, różnica pomiędzy n a 51/Program.cs
@@ -0,0 +1,49 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Podaj 3 cyfry");
+
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+
+if (n1 > 51)
+{
+    int n1D = (n1 - 51) * 3;
+    Console.WriteLine(n1D);
+
+}
+else
+{
+    int n1D = 51-n1;
+    Console.WriteLine(n1D);
+}
+
+if (n2 > 51)
+{
+    int n2D = (n2 - 51) * 3;
+    Console.WriteLine(n2D);
+
+}
+else
+{
+    int n2D = 51 - n2;
+    Console.WriteLine(n2D);
+}
+
+if (n3 > 51)
+{
+    int n3D = (n3 - 51) * 3;
+    Console.WriteLine(n3D);
+
+}
+else
+{
+    int n3D = 51 - n3;
+    Console.WriteLine(n3D);
+}
+
+
+
+
+
+Console.ReadKey();
