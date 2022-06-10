7b02bec695f32a529d5d8aed050815053c5b5b10
ZiemowitKorz <106156554+ZiemowitKorz@users.noreply.github.com>
2022-06-10 14:57:14 +0200

    Add files via upload

---
+++ Basics/CreatingStrings/CreatingStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution("abcd"));
+            Console.WriteLine(solution("abc"));
+            Console.WriteLine(solution("a"));
+            Console.ReadLine();
+
+        }
+        public static string solution(string str)
+        {
+            string result = string.Empty;
+
+            for(int i = 0; i<str.Length; i++)
+            {
+                result += str.Substring(0, i + 1);
+            }
+            return result;
+        }
+    }
+}
---
+++ Basics/EverySecondCharacter/EverySecondCharacter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution("Python"));
+            Console.WriteLine(solution("PHP"));
+            Console.WriteLine(solution("JS"));
+            Console.ReadLine();
+        }
+        public static string solution(string str)
+        {
+            string result = string.Empty;
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    result += str[i];
+                }
+               
+            }
+            return result;
+
+        }
+    }
+}
