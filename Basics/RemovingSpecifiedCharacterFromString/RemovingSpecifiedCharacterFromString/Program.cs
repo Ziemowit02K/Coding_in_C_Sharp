b401b6ed5039c397fa843a30c3e2207e779fc2c3
ZiemowitKorz <106156554+ZiemowitKorz@users.noreply.github.com>
2022-06-10 14:58:02 +0200

    Add files via upload

---
+++ Basics/RemovingSpecifiedCharacterFromString/RemovingSpecifiedCharacterFromString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution("xxHxix", "x"));
+            Console.WriteLine(solution("abxdddca", "a"));
+            Console.WriteLine(solution("xabjbhtrb", "b"));
+            Console.ReadLine();
+        }
+        public static string solution(string str, string unWanted)
+        {
+           
+            for(int i = str.Length-2; i > 0; i--)
+            {
+                if (str[i] == unWanted[0])
+                {
+                    str = str.Remove(i, 1);
+                }
+            }
+            return str;
+        }
+    }
+}
---
+++ Basics/WhatsAfterAnA/WhatsAfterAnA/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution("caabb"));
+            Console.WriteLine(solution("babaaba"));
+            Console.WriteLine(solution("aaaaa"));
+            Console.ReadLine();
+        }
+        public static bool solution(string str)
+        {
+            int count = 0;
+            for(int i=0;i<str.Length-1; i++)
+            {
+                if (str[i].Equals('a'))
+                {
+                    count++;
+                    if (str.Substring(i,2).Equals("aa") && count<2)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
