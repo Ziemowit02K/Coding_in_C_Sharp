17d3e3955884ccd253d90b32d2f54939a2eb368b
ZiemowitKorz <106156554+ZiemowitKorz@users.noreply.github.com>
2022-06-13 12:16:49 +0200

    Add files via upload

---
+++ Basics/ThreeGivenNumbersAreInIncreasingOrder/ThreeGivenNumbersAreInIncreasingOrder/Program.cs
@@ -0,0 +1,36 @@
+﻿// Write a C# Sharp program to check if three given numbers are in strict increasing order, such as 4 7 15, or 45, 56, 67, but not 4 ,5, 8 or 6, 6, 8.However,if a fourth parameter is true, equality is allowed, such as 6, 6, 8 or 7, 7, 7
+using System;
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(1, 2, 3, false));
+            Console.WriteLine(solution(1, 2, 3, true));
+            Console.WriteLine(solution(10, 2, 30, false));
+            Console.WriteLine(solution(10, 10, 30, true));
+            Console.ReadLine();
+        }
+        public static string solution(int x, int y,int z, bool str)
+        {
+            if (str)
+            {
+                if (x<=y && y<=z)
+                {
+                    return "True";
+                }
+                return "False";
+            }
+            else
+            {
+                if(x<y && y < z)
+                {
+                    return "True";
+                }
+
+            }
+            return "False";
+        }
+    }
+}
---
+++ Basics/TripleOFIntegers/TripleOFIntegers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(new[] {1,1,2,2,1}));
+            Console.WriteLine(solution(new[] {1,1,2,1,2,3}));
+            Console.WriteLine(solution(new[] {1,1,1,2,2,2,1}));
+            Console.ReadLine();
+        }
+        public static string solution(int[] numbers)
+        {
+            for(int i = 0; i < numbers.Length - 2; i++)
+            {
+                if(numbers[i] == numbers[i + 1] && numbers[i + 1] == numbers[i + 2])
+                {
+                    return "True";
+                }
+            }
+            return "False";
+        }
+    }
+}
