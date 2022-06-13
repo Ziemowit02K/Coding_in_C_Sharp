05b10d2526b3373f640886c97511b9053f318ebf
ZiemowitKorz <106156554+ZiemowitKorz@users.noreply.github.com>
2022-06-13 14:43:40 +0200

    Add files via upload

---
+++ Basics/SimpleLoginUserIdAndPassword/SimpleLoginUserIdAndPassword/Program.cs
@@ -0,0 +1,47 @@
+﻿//  Write a C# Sharp program that takes userid and password as input (type string). After 3 wrong attempts, user will be rejected.
+using System;
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Login , password;
+            int Tryes = 3;
+            do
+            {
+                Console.WriteLine("Enter yout UserID: ");
+                Login = Console.ReadLine();
+                Console.WriteLine("Enter yout Password: ");
+                password = Console.ReadLine();
+                if (Login == "root" && password == "1234")
+                {
+                    Tryes--;
+                    Console.WriteLine("");
+                    Console.WriteLine("Welcome");
+                    Console.WriteLine();
+                    Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    
+                    Tryes--;
+                    if (Tryes == 0)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You were rejected, try again later");
+                        Console.WriteLine("");
+                        Console.WriteLine("Click anything to continue");
+                        Console.ReadKey();
+                        break;
+                    }
+                    Console.Clear();
+                    Console.WriteLine("Try again");
+                    Console.WriteLine();
+                }
+            }
+            while (Tryes > 0);
+        }
+    }
+}
---
+++ Basics/TakesTheNumberAndWidthAlsoANumber/TakesTheNumberAndWidthAlsoANumber/Program.cs
@@ -0,0 +1,39 @@
+﻿//  Write a C# Sharp program that takes a number and a width also a number, as input and then displays a triangle of that width, using that number.
+using System;
+namespace exercises
+{
+    class Program
+    {
+        static void Main()
+        {
+            int NumberGiven;
+            int WidthGiven;
+            Console.WriteLine("Podaj numer do wyświetlenia: ");
+                NumberGiven = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj szerokość: ");
+                WidthGiven = Convert.ToInt32(Console.ReadLine());
+
+            int Width = WidthGiven;
+            int Height = Width;
+            
+
+            for (int i = 0; i < Height; i++)
+            {
+                for(int j = 0; j < Width; j++)
+                {
+                    Console.Write(NumberGiven);
+                    
+                }
+                Width--;
+                Console.WriteLine();
+            }
+
+
+
+            Console.ReadKey();
+        }
+
+            
+        }
+    }
+
