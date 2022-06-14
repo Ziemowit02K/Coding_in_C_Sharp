8860b5fc7b7f9b6af8ffb2b20de8a84051c75706
ZiemowitKorz <106156554+ZiemowitKorz@users.noreply.github.com>
2022-06-14 13:23:08 +0200

    Add files via upload

---
+++ DataTypeExercises/RadiusOFASphereAndCalculateAndDisplayTheSurface/RadiusOFASphereAndCalculateAndDisplayTheSurface/Program.cs
@@ -0,0 +1,37 @@
+﻿//Write a C# Sharp program that takes the radius of a sphere as input and calculate and display the surface and volume of the sphere
+
+using System;
+namespace exercise {
+    public class Program
+    {
+        public static void Main()
+        {
+            double pi = (3.14);
+            Console.Write("Input radius: ");
+            string radiusGiven = Console.ReadLine();
+            if (IsItNumber(radiusGiven))
+            {
+                double radius;
+                double.TryParse(radiusGiven, out radius);
+                double Volume = (4 * pi * radius * radius * radius)/3;
+                double Area = 4 * pi * radius * radius;
+
+                Console.WriteLine("Volume is: " + Volume);
+                Console.WriteLine("Area is: " + Area);
+
+            }
+    }
+        public static bool IsItNumber(string str)
+        {
+            double num;
+            if (double.TryParse(str, out num))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    } 
+}
---
+++ DataTypeExercises/TwoNumbersInputOperactionDisplayResult/TwoNumbersInputOperactionDisplayResult/Program.cs
@@ -0,0 +1,108 @@
+﻿// Write a C# Sharp program that takes two numbers as input and perform an operation (+,-,*,x,/) on them and displays the result of that operation
+using System;
+namespace exercises
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome! Here you can make simple calulations.");
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Firstly you need to input first number, then operation input like: (+,-,*,/)");
+                    Console.WriteLine("Next insert second number and get your result :)");
+                    Console.WriteLine("Press anything to go on");
+                    Console.ReadKey();
+                    Console.Clear();
+                            Console.WriteLine(" ");
+                            Console.WriteLine(" ");
+            Console.WriteLine("Insert first number: ");
+                string firstNumber1 = Console.ReadLine();
+            if(firstNumber1 == null || firstNumber1 == " " || firstNumber1 =="")
+            {
+                Console.Clear();
+                Console.WriteLine("Its not a number");
+                Console.ReadKey();
+
+
+            }
+            else 
+            {
+                int firstNumber = Convert.ToInt32(firstNumber1);
+                Console.WriteLine(" ");
+                Console.WriteLine("Insert sign of opperation: ");
+                string operationInput = Console.ReadLine().ToString();
+                Console.WriteLine(" ");
+                Console.WriteLine("Insert second number: ");
+                    string secondNumber1 = Console.ReadLine();
+                Console.WriteLine(" ");
+                if (secondNumber1 == null || secondNumber1 == " " || secondNumber1 == "")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Its not a number");
+                    Console.ReadKey();
+
+
+                }
+                else
+                {
+                    int secondNumber = Convert.ToInt32(secondNumber1);
+                    if (operationInput.Equals("+"))
+                    {
+                        int result = firstNumber + secondNumber;
+                        Console.WriteLine(firstNumber + " " + operationInput + " " + secondNumber + " = " + result);
+                    }
+                    else if (operationInput.Equals("-"))
+                    {
+                        if (firstNumber > secondNumber)
+                        {
+                            int result = firstNumber - secondNumber;
+                            Console.WriteLine(firstNumber + " " + operationInput + " " + secondNumber + " = " + result);
+                        }
+                        else
+                        {
+                            int result = secondNumber - firstNumber;
+                            Console.WriteLine(secondNumber + " " + operationInput + " " + firstNumber + " = " + result);
+                        }
+
+                    }
+                    else if (operationInput.Equals("*"))
+                    {
+                        int result = firstNumber * secondNumber;
+                        Console.WriteLine(secondNumber + " " + operationInput + " " + firstNumber + " = " + result);
+                    }
+                    else if (operationInput.Equals("/"))
+                    {
+                        int result = firstNumber / secondNumber;
+                        Console.WriteLine(secondNumber + " " + operationInput + " " + firstNumber + " = " + result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong input type");
+                    }
+                    Console.ReadLine();
+                }
+               
+            }
+            
+
+                       
+                    
+
+
+        }
+       /* public static bool IsItNumber(string str)
+        {
+            double number;
+            if (double.TryParse(str, out number))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+       */
+
+    }
+}
