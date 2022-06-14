17628b47eee9114c07ef1979df73e8db8545e2e8
Ziemowit Korzeniewski <korzeniewski.ziemowit@gmail.com>
2022-06-14 15:41:40 +0200

    Add files via upload

---
+++ LoopTraining/MultiplicationTableFrom1ToN/MultiplicationTableFrom1ToN/Program.cs
@@ -0,0 +1,61 @@
+﻿// Write a program in C# Sharp to display the multiplication table vertically from 1 to n.
+using System;
+namespace exercises
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello! Pls insert here number you want see multiplication of.");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.Write("Insert number for multiplication from 1 to: ");
+            string numbergiven = Console.ReadLine();
+
+            if (IsItNumber(numbergiven))
+            {
+                int number = Convert.ToInt32(numbergiven);
+                for(int i =1; i<=10; i++)
+                {
+                    for(int j=1; j <= number; j++)
+                    {
+                        
+                        if (j <= number - 1)
+                        {
+                            Console.Write("{0}x{1}={2}, ", i, j, i * j);
+                        }
+                        else
+                        {
+                            Console.Write("{0}x{1}={2}, ", i, j, i * j);
+                        }
+                        
+
+
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong input");
+            }
+        }
+        public static bool IsItNumber(string str)
+        {
+            int i = 0;
+            if(int.TryParse(str, out i))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
+
+        
---
+++ LoopTraining/Read10NumbersSumAndAverage/Read10NumbersSumAndAverage/Program.cs
@@ -0,0 +1,25 @@
+﻿// Write a program in C# Sharp to read 10 numbers from keyboard and find their sum and average.
+using System;
+namespace exercises
+{
+    class Program
+    {
+        static void Main()
+        {
+            int result = 0;
+            Console.WriteLine("Welcome!");
+            for(int i = 1; i < 11; i++)
+            {
+                Console.Write("Number-" + i + ": ") ;
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                result +=number;
+                
+            }
+            double average = result / 10;
+            Console.WriteLine("The sum of 10 numbers is: "+result);
+            Console.WriteLine("The Average is: " + average);
+        }
+        
+    }
+}
