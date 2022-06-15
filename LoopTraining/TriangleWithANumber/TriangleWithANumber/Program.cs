605bfa979b6dea16b4694bc93593112a599682b2
Ziemowit Korzeniewski <korzeniewski.ziemowit@gmail.com>
2022-06-15 09:55:53 +0200

    Add files via upload

---
+++ LoopTraining/TriangleWithANumber/TriangleWithANumber/Program.cs
@@ -0,0 +1,51 @@
+﻿// Write a program in C# Sharp to display the pattern like right angle triangle with a number.
+using System;
+namespace exercises
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, you need to insert number of rows in your future triangle");
+            Console.WriteLine();
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.Write("Insert number of rows:  ");
+            string numberGiven = Console.ReadLine();
+            if (IsItNumber(numberGiven))
+            {
+                int number = Convert.ToInt32(numberGiven);
+                for(int i = 1; i<=number; i++)
+                {
+                    int display = 1;
+                    for (int j = 1; j <= i; j++)
+                    {
+                        
+                        Console.Write(display);
+                        display++;
+                    }
+                    Console.WriteLine();
+                    display = 1;
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Wrong Input");
+                Console.ReadKey();
+            }
+        }
+        public static bool IsItNumber(string str)
+        {
+            int i;
+            if(int.TryParse(str,out i))
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
---
+++ LoopTraining/TriangleWithANumberIncreasedBy1/TriangleWithANumberIncreasedBy1/Program.cs
@@ -0,0 +1,52 @@
+﻿// Write a program in C# Sharp to make such a pattern like right angle triangle with number increased by 1
+using System;
+namespace exercises
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, you need to insert number of rows in your future triangle");
+            Console.WriteLine();
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.Write("Insert number of rows:  ");
+            string numberGiven = Console.ReadLine();
+            if (IsItNumber(numberGiven))
+            {
+                int display = 1;
+                int number = Convert.ToInt32(numberGiven);
+                for (int i = 1; i <= number; i++)
+                {
+                    
+                    for (int j = 1; j <= i; j++)
+                    {
+                        
+                        Console.Write(display+" ");
+                        display++;
+                    }
+                    Console.WriteLine();
+
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Wrong Input");
+                Console.ReadKey();
+            }
+        }
+        public static bool IsItNumber(string str)
+        {
+            int i;
+            if (int.TryParse(str, out i))
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
