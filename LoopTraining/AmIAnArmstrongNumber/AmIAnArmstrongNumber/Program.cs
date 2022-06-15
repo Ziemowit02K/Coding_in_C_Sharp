4d3d6347ed1c432fbbc5d54fd84f307ee9b41e1c
Ziemowit Korzeniewski <korzeniewski.ziemowit@gmail.com>
2022-06-15 14:31:13 +0200

    Add files via upload

---
+++ LoopTraining/AmIAnArmstrongNumber/AmIAnArmstrongNumber/Program.cs
@@ -0,0 +1,65 @@
+﻿//Write a C# Sharp program to check whether a given number is an Armstrong number or not. 
+using System;
+namespace exercises
+{
+    class Program
+    {
+        static void Main()
+        {
+            Welcome();
+
+            Console.Write("Insert a number: ");
+            string numberGiven = Console.ReadLine();
+            if (IsItNumber(numberGiven))
+            {
+                int num = Convert.ToInt32(numberGiven);
+                int sum = 0;
+                int num1=0;
+                int r = 0;
+                for (num1 = num; num != 0; num = num / 10)
+                {
+                    r = num % 10;
+                    sum = sum + (r * r * r);
+                }
+                if (sum == num1)
+                {
+                    Console.WriteLine("{0} is an Armstrong number", num1);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not an Armstrong number", num1);
+                }
+            }
+            else
+            {
+                Console.WriteLine("WrongInput");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+        }
+        public static bool IsItNumber(string str)
+        {
+            if(int.TryParse(str,out int i))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static void Welcome()
+        {
+                    Console.WriteLine("                     --- --- --- Hello! --- --- ---");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("        Today I am gonna tell you is your number an ARMSTRONG number");
+                    Console.WriteLine();
+                    Console.WriteLine("               -- -- -- Press any key to contionue -- -- --   ");
+                    Console.ReadKey();
+                    Console.Clear();
+            
+                
+            }
+        }
+    }
+
+
---
+++ LoopTraining/DisplayingPatternLikeAdiamond/DisplayingPatternLikeAdiamond/Program.cs
@@ -0,0 +1,76 @@
+﻿//Write a program in C# Sharp to display the pattern like a diamond
+using System;
+namespace exercises
+{
+    class Program
+    {
+        static void Main()
+        {
+            Welcome();
+
+            Console.Write("Insert how much '*' wide schould it be: ");
+            string numberGiven = Console.ReadLine();
+
+            if (IsItNumber(numberGiven))
+            {
+                
+                int number = Convert.ToInt32(numberGiven);
+                for (int i= 0; i <= number; i++)
+                {
+                    for(int k = 1; k <= number-i; k++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for(int j = 1; j <= 2 * i - 1; j++)
+                    {
+                        Console.Write('*');
+                    }
+                    Console.WriteLine();
+                }
+                for (int i = number - 1; i >= 1; i--)
+                {
+                    for (int j = 1; j <= number - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    
+                    for(int j = 1; j <= 2 * i - 1; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("WrongInput");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+        }
+        public static bool IsItNumber(string str)
+        {
+            if (int.TryParse(str, out int i))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static void Welcome()
+        {
+            Console.WriteLine("                     --- --- --- Hello! --- --- ---");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("        Today I am gonna show you the biggest diamond you have ever seen");
+            Console.WriteLine();
+            Console.WriteLine("               -- -- -- Press any key to contionue -- -- --   ");
+            Console.ReadKey();
+            Console.Clear();
+
+
+        }
+    }
+}
+
+
