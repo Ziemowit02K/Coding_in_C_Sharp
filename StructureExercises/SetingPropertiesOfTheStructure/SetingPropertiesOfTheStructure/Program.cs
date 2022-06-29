cae0e9a09d80854b055650b016c1d61bfb1dfed6
Ziemowit Korzeniewski <korzeniewski.ziemowit@gmail.com>
2022-06-29 13:59:58 +0200

    Add files via upload

---
+++ StructureExercises/SetingPropertiesOfTheStructure/SetingPropertiesOfTheStructure/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Exercises
+{
+    class Program
+    {
+        struct newStruct
+        {
+            private int num;
+
+            public int n
+            {
+                get 
+                {
+                    return num;
+                }
+                set 
+                {
+                    if(value<50)
+                    num = value; 
+                }
+            }
+            public void clsMethod()
+            {
+                Console.WriteLine("The stored value is: {0}",num);
+            }
+        }
+        class strucExercises
+        {
+            public static void Main()
+            {
+                Console.WriteLine("Declares a structure with a property, a method and a private field: ");
+                newStruct Example = new newStruct();
+                Example.n = 15;
+                Example.clsMethod();
+            }
+        }
+    }
+
+}
---
+++ StructureExercises/SructuresToMethods/SructuresToMethods/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace exercises
+{
+    class newClass
+    {
+        public int n;
+    }
+    struct newStruct
+    {
+        public int n;
+    }
+    class strucExercises
+    {
+        public static void trackStruct(newStruct st)
+        {
+            st.n = 8;
+        }
+        public static void tracClass(newClass cl)
+        {
+            cl.n = 8;
+        }
+        public static void Main()
+        {
+            Console.WriteLine("When a structure and a class instance is passed to a method");
+            Console.WriteLine();
+            newStruct ns =new newStruct();
+            newClass nc = new newClass();
+            ns.n = 5;
+            nc.n = 5;
+
+            trackStruct(ns);
+            tracClass(nc);
+            Console.WriteLine("ns.n={0}",ns.n);
+            Console.WriteLine("nc.n={0}",nc.n);
+        }
+    }
+}
