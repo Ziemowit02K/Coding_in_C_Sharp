cc92b8285a4125ccdc37f97dd77e86e72b5e10e9
Ziemowit Korzeniewski <korzeniewski.ziemowit@gmail.com>
2022-06-29 14:03:43 +0200

    Add files via upload

---
+++ StructureExercises/DemonstratesStructureInitializationWithoutOperator/DemonstratesStructureInitializationWithoutOperator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace exercises
+{
+    public struct newStruct
+    {
+        public int m, n;
+        public newStruct(int p1, int p2)
+        {
+            m = p1; n = p1;
+        }
+    }
+    class StructureExercises
+    {
+        static void Main()
+        {
+            Console.WriteLine("Structure declares using default and parametrized construtors");
+            Console.WriteLine();
+
+            newStruct myPoint;
+            myPoint.m = 30;
+            myPoint.n = 40;
+
+            Console.WriteLine("m = {0}, n = {1}",myPoint.m,myPoint.n);
+
+            Console.ReadKey();
+
+        }
+    }
+}
